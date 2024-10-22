808ec356e42073a020f9690da952550c110b8730
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-10-22 19:23:38 -0300

    Criação do projeto API Produtos

---
+++ src/main/java/io/github/com/devcarlos/produtosapi/ProdutosApiApplication.java
@@ -0,0 +1,21 @@
+package io.github.com.devcarlos.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProdutosApiApplication {
+
+	@GetMapping("/hello-word")
+	public String helloWord(){
+		return "Hello Word";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+}
