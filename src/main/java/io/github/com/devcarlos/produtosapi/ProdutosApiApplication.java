584d874063f1544b281c5dd231ae032157a91065
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-11-08 21:29:54 -0300

    Criando endpoint para recebimento dos produtos

--- src/main/java/io/github/com/devcarlos/produtosapi/ProdutosApiApplication.java
+++ src/main/java/io/github/com/devcarlos/produtosapi/ProdutosApiApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RestController
 @SpringBootApplication
-@RestController
 public class ProdutosApiApplication {
 
-	@GetMapping("/hello-word")
-	public String helloWord(){
-		return "Hello Word";
-	}
+//	@GetMapping("/hello-word")
+//	public String helloWord(){
+//		return "Hello Word";
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
